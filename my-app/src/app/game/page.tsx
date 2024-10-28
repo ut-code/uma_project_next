0c5dfa341d7f17a46f785845479b77cd38517e8c
KissShot-K <chuanyeharuto4@gmail.com>
2024-10-28 19:27:38 +0900

    Update page.tsx

--- my-app/src/app/game/page.tsx
+++ my-app/src/app/game/page.tsx
@@ -3,8 +3,37 @@
 import Link from "next/link"
 import { HomeIcon, ArrowDown, Activity } from "lucide-react"
 import { motion } from "framer-motion"
+import { div, p } from "framer-motion/client";
+import { useEffect, useState } from "react";
 
-export default function gamePage() {
+export default function Game() {
+  const [randomRaceData, setRandomRaceData] = useState<any[]>([])
+
+  useEffect(() => {
+    const fetchRandomRaceData = async() => {
+      try {
+        const response = await fetch('/api/game');
+        if (!response.ok) {
+          throw new Error(`HTTPのエラー: ${response.status}`);
+        }
+        const data = await response.json();
+        console.log("取得したデータ:", data);
+        setRandomRaceData(prevData => [...prevData, data]);
+      } catch (error) {
+        console.error('データの取得に失敗しました:', error);
+      }
+    };
+
+    fetchRandomRaceData();
+    fetchRandomRaceData();
+    fetchRandomRaceData();
+    console.log('randomRaceData:', randomRaceData)
+    }, [])
+
+    useEffect(() => {
+      console.log('randomRaceData:', randomRaceData);
+    }, [randomRaceData]);
+
     return(
         <div>
             <div className="fixed top-4 left-4"> {/* 画面の左上に固定 */}
@@ -75,6 +104,19 @@ export default function gamePage() {
         </motion.div>
         </motion.div>
         </div>
+            <div>
+              { randomRaceData.length > 0 ? (
+                randomRaceData.map((raceData, index) => (
+                  <div key={index}>
+                    <p>{raceData.title}</p>
+                    <p>{raceData.course}</p>
+                    <a href={raceData.url}>{raceData.url}</a>
+                  </div>
+                ))
+              ) : (
+                <p>データを読み込み中...</p>
+              )}
+            </div>
         </div>
     )
 }
