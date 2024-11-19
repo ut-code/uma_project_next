702477adeb7933a4e4a72c9a684878801968ba1c
ROBOT <tkata12@outlook.jp>
2024-11-19 18:17:25 +0900

    ランキング表示用のAPIを追加

--- src/app/api/ranking/route.ts
+++ src/app/api/ranking/route.ts
@@ -1,7 +1,14 @@
 import { NextApiRequest } from "next";
 import { NextResponse } from "next/server";
+import { z } from "zod";
 
 import { db } from "@/server/database";
+import { structChecker } from "@/server/struct";
+
+const _RequestZod = z.object({
+    name: z.string(),
+    score: z.number(),
+})
 
 export async function GET() {
     const datas = await db.ranking.findMany({
@@ -10,5 +17,38 @@ export async function GET() {
         },
         take: 40,
     });
-    return NextResponse.json(datas);
+    return NextResponse.json(datas, { status: 200 });
+}
+
+export async function POST(req: NextApiRequest) {
+    const body = structChecker(req.body, _RequestZod);
+    if (!body) {
+        return NextResponse.json({
+            content: "BODY_PARSE_ERR"
+        }, { status: 400 });
+    }
+    const element = await db.ranking.findFirst({
+        where: body,
+    });
+    if (!element) {
+        await db.ranking.create({
+            data: body
+        })
+    }
+    const elements = await db.ranking.findMany({
+        orderBy: {
+            score: "desc"
+        },
+    });
+    for (let i = 0; i < elements.length; i++) {
+        const data = elements[i];
+        if (data.name === body.name && data.score === BigInt(body.score)) {
+            return NextResponse.json({
+                rank: i + 1,
+            }, { status: 200 })
+        }
+    }
+    return NextResponse.json({
+        rank: null,
+    }, { status: 202 })
 }
